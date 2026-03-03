eda3dbc6b7ada4eecff00baa788a387b9af7b7ee
Isa <isarahatbekov250@gmail.com>
2026-03-03 12:15:22 +0600

    adaptive of header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,21 @@
-import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
-import AboutUs from '../components/sections/AboutUs/AboutUs'
-import Form from '../components/sections/Form/Form'
-import Hero from '../components/sections/Hero/Hero'
-import OurClients from '../components/sections/OurClients/OurClients'
-import Production from '../components/sections/Production/Production'
-import ReadyProjects from '../components/sections/ReadyProjects/ReadyProjects'
-import SpecialConditions from '../components/sections/SpecialConditions/SpecialConditions'
-import WhyWe from '../components/sections/WhyWe/WhyWe'
-import WorkProcess from '../components/sections/WorkProcess /WorkProcess'
 
 export default function Home() {
 	return (
-		<div>
+		<div className='page-layout'>
 			<Header />
 			<main>
-				<Hero />
-				<AboutUs />
-				<OurClients />
-				<Production />
-				<WorkProcess />
-				<WhyWe />
-				<ReadyProjects />
-				<SpecialConditions />
-				<Form />
+				{/* <Hero /> */}
+				{/* <AboutUs /> */}
+				{/* <OurClients /> */}
+				{/* <Production /> */}
+				{/* <WorkProcess /> */}
+				{/* <WhyWe /> */}
+				{/* <ReadyProjects /> */}
+				{/* <SpecialConditions /> */}
+				{/* <Form /> */}
 			</main>
-			<Footer />
+			{/* <Footer /> */}
 		</div>
 	)
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,41 @@
+'use client'
 import Image from 'next/image'
 import styles from './Header.module.scss'
 
-import logo from '@/public/images/logo.png'
+import logo from '@/public/images/logo.svg'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden'
+		} else {
+			document.body.style.overflow = ''
+		}
+
+		const handleResize = () => {
+			if (window.innerWidth > 768 && isOpen) {
+				setIsOpen(false)
+			}
+		}
+		window.addEventListener('resize', handleResize)
+
+		return () => {
+			document.body.style.overflow = ''
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [isOpen])
+
+	const handleOpen = () => {
+		setIsOpen(!isOpen)
+	}
+
+	const closeMenu = () => {
+		setIsOpen(false)
+	}
+
 	return (
 		<header className={styles.header}>
 			<div className={`${styles.container} container`}>
@@ -48,8 +80,65 @@ const Header = () => {
 							</a>
 						</li>
 					</ul>
-
 					<button className={styles.btn}>Рассчитать стоимость</button>
+					<button className={styles.burger} onClick={handleOpen}>
+						<span
+							className={`${styles.burgerLine} ${isOpen ? styles.open : ''}`}
+						></span>
+					</button>
+					<div className={`${styles.burgerMenu} ${isOpen ? styles.open : ''}`}>
+						<ul className={styles.burgerList}>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									О нас
+								</a>
+							</li>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									Наши клиенты
+								</a>
+							</li>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									Что производим
+								</a>
+							</li>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									Как работаем
+								</a>
+							</li>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									Преимущества
+								</a>
+							</li>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									Портфолио
+								</a>
+							</li>
+							<li className={styles.item}>
+								<a className={styles.link} href='#' onClick={closeMenu}>
+									Партнёрам
+								</a>
+							</li>
+							<li className={styles.burgerBtnsWrapper}>
+								<button
+									className={`${styles.burgerBtn} ${styles.burgerBtnPrimary}`}
+									onClick={closeMenu}
+								>
+									Рассчитать стоимость
+								</button>
+								<button
+									className={`${styles.burgerBtn} ${styles.burgerBtnTransparent}`}
+									onClick={closeMenu}
+								>
+									Получить консультацию
+								</button>
+							</li>
+						</ul>
+					</div>
 				</nav>
 			</div>
 		</header>
